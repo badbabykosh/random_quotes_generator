56568e82f0d4940897c13c080c83b7fe806efec6
thekosh <badbabykosh.com>
2015-09-30 12:35:16 -0700

    jsonp working

--- js/src/Random.js
+++ js/src/Random.js
@@ -4,14 +4,18 @@ function RandomInit(){
 
 RandomInit.prototype.quote = function(){
   (function() {
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?")
+    //usefull to test agains a jsonp end point
+    //$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?")
+    //$.getJSON("https://koshspeaks.herokuapp.com/api/v1/episodes")
+    $.getJSON("localhost:3000/api/v1/episodes.jsonp?callback=parseEpisodes")
         .done(function (json) {
-          console.log('json: '+json.quoteText);
+          console.log('json: '+json);
           document.getElementById("quote").innerHTML = json.quoteText;
           //TODO how to return value from here?
           //return json.quoteText;
         })
-        .fail(function(jqxhr,textStatus,error){
+        .fail(function(jqxhr,textStatus,error,json){
+          console.log('json: '+json);
           var err = textStatus +','+error;
           console.log('Shiz got broke: '+ err);
         });
