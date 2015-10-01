12b9c1270dbd05b3e28344b95320cd7a678ed4dc
thekosh <badbabykosh.com>
2015-10-01 16:15:47 -0700

    wip-working new api end point

--- js/src/Random.js
+++ js/src/Random.js
@@ -6,11 +6,13 @@ RandomInit.prototype.quote = function(){
   (function() {
     //usefull to test agains a jsonp end point
     //$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?")
-    //$.getJSON("https://koshspeaks.herokuapp.com/api/v1/episodes")
-    $.getJSON("localhost:3000/api/v1/episodes.jsonp?callback=parseEpisodes")
+    $.getJSON("http://localhost:3000/api/v1/episodes")
+    //$.getJSON("http://localhost:3000/api/v1/episodes.jsonp?callback=parseEpisodes")
+    //$.getJSON("https://koshspeaks.herokuapp.com/api/v1/episodes.jsonp?callback=parseEpisodes")
         .done(function (json) {
-          console.log('json: '+json);
-          document.getElementById("quote").innerHTML = json.quoteText;
+          //TODO this code should resemble forismatic....should only be json.getContent
+          console.dir('json: '+json.episodes[0].quotes[0].content);
+          document.getElementById("quote").innerHTML = json.episodes[0].quotes[0].content
           //TODO how to return value from here?
           //return json.quoteText;
         })
